a50dc688fdaa0df6c3a59beb4629829f4e4c91df
Kamilucha <brnkamila2@gmail.com>
2023-05-07 11:50:47 +0300

    task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const storageKey = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const throttleMeth = throttle((seconds) => {
+    console.log(seconds)
+    saveValue(seconds);
+}, 1000)
+
+player.on('timeupdate', ({ seconds }) => {
+    throttleMeth(seconds)  
+});
+
+function saveValue(value) {
+    localStorage.setItem(storageKey, value)
+};
+
+function checkStoredTime() {
+    const currTime = localStorage.getItem(saveValue);
+
+    if (currTime) {
+        player.setCurrentTime(currTime)
+    }
+};
+
+checkStoredTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    textareaEl: document.querySelector('textarea'),
+    inputEl: document.querySelector('input')
+};
+
+// refs.textareaEl.addEventListener('input', (e) => console.log(e))
+
+
+const throttleMeth = throttle(getValue, 500);
+refs.formEl.addEventListener('input', throttleMeth)
+
+function getValue() {
+     console.log(refs.inputEl.value, refs.textareaEl.value)
+
+    localStorage.setItem(feedbackKey, JSON.stringify({
+        email: refs.inputEl.value,
+        message: refs.textareaEl.value
+    }))
+}
+refs.formEl.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log('ляляля тапаля');
+    e.currentTarget.reset();
+
+    localStorage.removeItem(feedbackKey);
+}
+
+const feedbackKey = "feedback-form-state"
+
+// function loh(value) {
+//     localStorage.setItem(feedbackKey, JSON.stringify(value))
+// };
+
+function getStorageValue() { 
+    const value = localStorage.getItem(feedbackKey);
+
+    if (value) {
+        const parse = JSON.parse(value)
+        refs.inputEl.value = parse.email;
+        refs.textareaEl.value = parse.message;
+    }
+    console.log(value ,JSON.parse(value))
+}
+
+getStorageValue();
 
